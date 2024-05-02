2c42f5c059e4220da54ad4c3eb9e1d03dd687731
Utku Ozdemir <utku.ozdemir@siderolabs.com>
2024-05-02 12:09:38 +0200

    feat: add button to overview page to download partial machine config
    So that users can join a vanilla Talos to Omni by applying this machine configuration.
    
    Closes siderolabs/omni#171.
    
    Signed-off-by: Utku Ozdemir <utku.ozdemir@siderolabs.com>

--- frontend/src/views/omni/Overview/Overview.vue
+++ frontend/src/views/omni/Overview/Overview.vue
@@ -109,11 +109,9 @@ included in the LICENSE file.
           <div class="flex-col space-y-6">
             <div class="overview-card p-6 flex flex-col gap-5 place-items-stretch" style="width: 350px">
               <div class="text-naturals-N14 text-sm">Add Machines</div>
-              <t-button icon="long-arrow-down" iconPosition="left" @click="() => openDownloadIso()">Download
-                Installation Media
-              </t-button>
-              <t-button icon="copy" iconPosition="left" @click="() => copyValue(items[0]?.spec?.args)">Copy Kernel
-                Parameters</t-button>
+              <t-button icon="long-arrow-down" iconPosition="left" @click="() => openDownloadIso()">Download Installation Media</t-button>
+              <t-button icon="long-arrow-down" iconPosition="left" @click="() => downloadMachineJoinConfig(items[0]?.spec)">Download Machine Join Config</t-button>
+              <t-button icon="copy" iconPosition="left" @click="() => copyValue(items[0]?.spec?.args)">Copy Kernel Parameters</t-button>
             </div>
             <div class="overview-card p-6 flex flex-col gap-5 place-items-stretch" style="width: 350px">
               <div class="text-naturals-N14 text-sm">CLI</div>
@@ -137,16 +135,16 @@ included in the LICENSE file.
 import { useRouter } from "vue-router";
 import pluralize from "pluralize";
 import {
-  ConnectionParamsType,
   ClusterStatusType,
-  SysVersionType,
-  SysVersionID,
-  EphemeralNamespace,
-  DefaultNamespace,
   ConfigID,
-  RoleNone,
-  MachineStatusMetricsType,
+  ConnectionParamsType,
+  DefaultNamespace,
+  EphemeralNamespace,
   MachineStatusMetricsID,
+  MachineStatusMetricsType,
+  RoleNone,
+  SysVersionID,
+  SysVersionType,
 } from "@/api/resources";
 import { computed, ref } from "vue";
 import { copyText } from "vue3-clipboard";
@@ -155,7 +153,7 @@ import { Runtime } from "@/api/common/omni.pb";
 import { Resource } from "@/api/grpc";
 import { itemID } from "@/api/watch";
 import { MachineStatusMetricsSpec } from "@/api/omni/specs/omni.pb";
-import { downloadTalosconfig, downloadOmniconfig } from "@/methods";
+import { downloadOmniconfig, downloadTalosconfig } from "@/methods";
 
 import OverviewCircleChartItem from "@/views/cluster/Overview/components/OverviewCircleChart/OverviewCircleChartItem.vue";
 import TButton from "@/components/common/Button/TButton.vue";
@@ -164,6 +162,7 @@ import ClusterStatus from "@/views/omni/Clusters/ClusterStatus.vue";
 import Watch from "@/components/common/Watch/Watch.vue";
 import PageHeader from "@/components/common/PageHeader.vue";
 import { canCreateClusters, canReadClusters, currentUser } from "@/methods/auth";
+import { ConnectionParamsSpec } from "@/api/omni/specs/siderolink.pb";
 
 const hasRoleNone = computed(() => {
   const role = currentUser.value?.spec?.role
@@ -226,6 +225,49 @@ const downloadTalosctl = () => {
   router.push({
     query: { modal: "downloadTalosctlBinaries" },
   });
+};
+
+const downloadMachineJoinConfig = (item?: ConnectionParamsSpec) => {
+  if (!item?.args) {
+    return;
+  }
+
+  // parse kernel args to extract the values for the template
+  const args = item.args.split(" ");
+  const argsMap = args.reduce((acc, arg) => {
+    const [key, ...vals] = arg.split("=");
+    acc[key] = vals.join("=");
+    return acc;
+  }, {});
+
+  const apiUrl = argsMap["siderolink.api"];
+  const talosEventsSink = argsMap["talos.events.sink"];
+  const talosLoggingKernel = argsMap["talos.logging.kernel"];
+
+  const config = `apiVersion: v1alpha1
+kind: SideroLinkConfig
+apiUrl: "${apiUrl}"
+---
+apiVersion: v1alpha1
+kind: EventSinkConfig
+endpoint: "${talosEventsSink}"
+---
+apiVersion: v1alpha1
+kind: KmsgLogConfig
+name: omni-kmsg
+url: "${talosLoggingKernel}"
+`
+
+  const element = document.createElement("a");
+  element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(config));
+  element.setAttribute("download", "machine-config.yaml");
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
 };
 </script>
 
