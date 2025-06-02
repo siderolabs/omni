siderolabs/omni

commit 7c17ed6cf8b066877618c94712cdfd2131c4b574

fix: use the correct schematic IP for maintenance upgrades
Maintenance upgrades triggered from the UI were using the wrong schematic ID, causing the machines which use UKI to lose siderolink kernel args and disconnect.

Since we have a complex logic to build the correct install image including the schematic, move it to a central place.

Add a new management endpoint for the maintenance upgrades. UI now calls this endpoint instead of calling the Talos API directly.
The new endpoint builds the install image correctly using the common logic and issues the upgrade.

Signed-off-by: Utku Ozdemir <utku.ozdemir@siderolabs.com>

internal/backend/grpc/management.go
@@ -8,6 +8,7 @@ package grpc
 import (
 	"bytes"
 	"context"
+	"crypto/tls"
 	"errors"
 	"fmt"
 	"io"
@@ -25,6 +26,7 @@ import (
 	"github.com/siderolabs/go-kubernetes/kubernetes/manifests"
 	"github.com/siderolabs/go-kubernetes/kubernetes/upgrade"
 	"github.com/siderolabs/talos/pkg/machinery/api/common"
+	"github.com/siderolabs/talos/pkg/machinery/client"
 	"github.com/zitadel/oidc/v3/pkg/op"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -44,6 +46,7 @@ import (
 	"github.com/siderolabs/omni/internal/backend/dns"
 	"github.com/siderolabs/omni/internal/backend/grpc/router"
 	"github.com/siderolabs/omni/internal/backend/imagefactory"
+	"github.com/siderolabs/omni/internal/backend/installimage"
 	"github.com/siderolabs/omni/internal/backend/runtime"
 	"github.com/siderolabs/omni/internal/backend/runtime/kubernetes"
 	"github.com/siderolabs/omni/internal/backend/runtime/omni"
@@ -72,15 +75,8 @@ type JWTSigningKeyProvider interface {
 	SigningKey(ctx context.Context) (op.SigningKey, error)
 }
 
-func newManagementServer(
-	omniState state.State,
-	jwtSigningKeyProvider JWTSigningKeyProvider,
-	logHandler *siderolink.LogHandler,
-	logger *zap.Logger,
-	dnsService *dns.Service,
-	imageFactoryClient *imagefactory.Client,
-	auditor AuditLogger,
-	omniconfigDest string,
+func newManagementServer(omniState state.State, jwtSigningKeyProvider JWTSigningKeyProvider, logHandler *siderolink.LogHandler, logger *zap.Logger,
+	dnsService *dns.Service, imageFactoryClient *imagefactory.Client, auditor AuditLogger, omniconfigDest string,
 ) *managementServer {
 	return &managementServer{
 		omniState:             omniState,
@@ -674,6 +670,68 @@ func (s *managementServer) ReadAuditLog(req *management.ReadAuditLogRequest, srv
 	}
 
 	return closeFn()
+}
+
+// MaintenanceUpgrade performs a maintenance upgrade on a specified machine.
+func (s *managementServer) MaintenanceUpgrade(ctx context.Context, req *management.MaintenanceUpgradeRequest) (*management.MaintenanceUpgradeResponse, error) {
+	s.logger.Info("maintenance upgrade request received", zap.String("machine_id", req.MachineId), zap.String("version", req.Version))
+
+	if _, err := s.authCheckGRPC(ctx, auth.WithRole(role.Operator)); err != nil {
+		return nil, err
+	}
+
+	if req.MachineId == "" {
+		return nil, status.Error(codes.InvalidArgument, "machine id is required")
+	}
+
+	ctx = actor.MarkContextAsInternalActor(ctx)
+
+	machineStatus, err := safe.StateGetByID[*omnires.MachineStatus](ctx, s.omniState, req.MachineId)
+	if err != nil {
+		if state.IsNotFoundError(err) {
+			return nil, status.Error(codes.NotFound, "machine not found")
+		}
+
+		return nil, fmt.Errorf("failed to get machine status: %w", err)
+	}
+
+	if !machineStatus.TypedSpec().Value.Maintenance {
+		return nil, status.Error(codes.FailedPrecondition, "machine is not in maintenance mode")
+	}
+
+	machineConfigGenOptions, err := safe.ReaderGetByID[*omnires.MachineConfigGenOptions](ctx, s.omniState, req.MachineId)
+	if err != nil {
+		if state.IsNotFoundError(err) {
+			return nil, status.Error(codes.NotFound, "machine config gen options not found")
+		}
+
+		return nil, fmt.Errorf("failed to get machine config gen options: %w", err)
+	}
+
+	installImage := machineConfigGenOptions.TypedSpec().Value.InstallImage
+	installImage.TalosVersion = req.Version
+
+	installImageStr, err := installimage.Build(s.imageFactoryClient.Host(), req.MachineId, installImage)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build install image: %w", err)
+	}
+
+	s.logger.Info("built maintenance upgrade image", zap.String("image", installImageStr))
+
+	address := machineStatus.TypedSpec().Value.ManagementAddress
+	opts := talos.GetSocketOptions(address)
+	opts = append(opts, client.WithTLSConfig(&tls.Config{InsecureSkipVerify: true}), client.WithEndpoints(address))
+
+	talosClient, err := client.New(ctx, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create talos client: %w", err)
+	}
+
+	if _, err = talosClient.UpgradeWithOptions(ctx, client.WithUpgradeImage(installImageStr)); err != nil {
+		return nil, fmt.Errorf("failed to upgrade machine: %w", err)
+	}
+
+	return &management.MaintenanceUpgradeResponse{}, nil
 }
 
 func parseTime(date string, fallback time.Time) (time.Time, error) {

internal/backend/installimage/installimage.go
@@ -1,0 +1,74 @@
+// Copyright (c) 2025 Sidero Labs, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+
+// Package installimage provides utilities to work with the install images.
+package installimage
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/blang/semver/v4"
+	"github.com/cosi-project/runtime/pkg/resource"
+
+	"github.com/siderolabs/omni/client/api/omni/specs"
+	appconfig "github.com/siderolabs/omni/internal/pkg/config"
+)
+
+// Build builds the install image for the provided properties.
+func Build(imageFactoryHost string, resID resource.ID, installImage *specs.MachineConfigGenOptionsSpec_InstallImage) (string, error) {
+	if imageFactoryHost == "" {
+		return "", fmt.Errorf("image factory host is not set")
+	}
+
+	if !installImage.SchematicInitialized {
+		return "", fmt.Errorf("machine %q has no schematic information set", resID)
+	}
+
+	schematicID := installImage.SchematicId
+
+	securityState := installImage.SecurityState
+	if securityState == nil { // should never happen - must have been handled before entering this function
+		return "", fmt.Errorf("machine %q has no secure boot status set", resID)
+	}
+
+	installerName := "installer"
+	if securityState.SecureBoot {
+		installerName = "installer-secureboot"
+	}
+
+	if installImage.SchematicInvalid {
+		schematicID = ""
+	}
+
+	desiredTalosVersion := installImage.TalosVersion
+
+	if desiredTalosVersion == "" {
+		return "", fmt.Errorf("machine %q has no talos version set", resID)
+	}
+
+	version, err := semver.ParseTolerant(desiredTalosVersion)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse Talos version %q: %w", desiredTalosVersion, err)
+	}
+
+	if version.Major >= 1 && version.Minor >= 10 { // prepend the platform to the installer name for Talos 1.10+
+		if installImage.Platform == "" {
+			return "", fmt.Errorf("machine %q has no platform set", resID)
+		}
+
+		installerName = installImage.Platform + "-" + installerName
+	}
+
+	if !strings.HasPrefix(desiredTalosVersion, "v") {
+		desiredTalosVersion = "v" + desiredTalosVersion
+	}
+
+	if schematicID != "" {
+		return imageFactoryHost + "/" + installerName + "/" + schematicID + ":" + desiredTalosVersion, nil
+	}
+
+	return appconfig.Config.TalosRegistry + ":" + desiredTalosVersion, nil
+}

.github/workflows/ci.yaml
@@ -1,6 +1,6 @@
 # THIS FILE WAS AUTOMATICALLY GENERATED, PLEASE DO NOT EDIT.
 #
-# Generated on 2025-02-18T13:59:57Z by kres 8a48729.
+# Generated on 2025-05-22T17:54:46Z by kres 9f64b0d.
 
 name: default
 concurrency:
@@ -174,7 +174,7 @@ jobs:
           INTEGRATION_RUN_E2E_TEST: "true"
           INTEGRATION_TEST_ARGS: --test.run CleanState/|Auth/|DefaultCluster/|CLICommands/
           RUN_TALEMU_TESTS: "true"
-          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceDowngrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
+          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceUpgrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
           WITH_DEBUG: "true"
         run: |
           sudo -E make run-integration-test
@@ -205,15 +205,15 @@ jobs:
             !_out/etcd
             !_out/secondary-storage/**
           retention-days: "5"
-      - name: save-talos-logs-artifacts
+      - name: save-integration-test-artifacts
         if: always()
         uses: actions/upload-artifact@v4
         with:
-          name: talos-logs
+          name: integration-test
           path: |-
             ~/.talos/clusters/**/*.log
             !~/.talos/clusters/**/swtpm.log
-            /tmp/test-logs
+            /tmp/integration-test
           retention-days: "5"
       - name: Generate Checksums
         if: startsWith(github.ref, 'refs/tags/')
@@ -299,19 +299,19 @@ jobs:
           INTEGRATION_RUN_E2E_TEST: "false"
           INTEGRATION_TEST_ARGS: --test.run CleanState/|EtcdBackupAndRestore
           RUN_TALEMU_TESTS: "false"
-          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceDowngrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
+          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceUpgrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
           WITH_DEBUG: "true"
         run: |
           sudo -E make run-integration-test
-      - name: save-talos-logs-artifacts
+      - name: save-integration-test-artifacts
         if: always()
         uses: actions/upload-artifact@v4
         with:
-          name: talos-logs-e2e-backups
+          name: integration-test-e2e-backups
           path: |-
             ~/.talos/clusters/**/*.log
             !~/.talos/clusters/**/swtpm.log
-            /tmp/test-logs
+            /tmp/integration-test
           retention-days: "5"
         continue-on-error: true
   e2e-forced-removal:
@@ -378,19 +378,19 @@ jobs:
           INTEGRATION_RUN_E2E_TEST: "false"
           INTEGRATION_TEST_ARGS: --test.run CleanState/|ForcedMachineRemoval/|ReplaceControlPlanes/|ConfigPatching/|KubernetesNodeAudit/
           RUN_TALEMU_TESTS: "false"
-          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceDowngrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
+          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceUpgrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
           WITH_DEBUG: "true"
         run: |
           sudo -E make run-integration-test
-      - name: save-talos-logs-artifacts
+      - name: save-integration-test-artifacts
         if: always()
         uses: actions/upload-artifact@v4
         with:
-          name: talos-logs-e2e-forced-removal
+          name: integration-test-e2e-forced-removal
           path: |-
             ~/.talos/clusters/**/*.log
             !~/.talos/clusters/**/swtpm.log
-            /tmp/test-logs
+            /tmp/integration-test
           retention-days: "5"
         continue-on-error: true
   e2e-scaling:
@@ -457,19 +457,19 @@ jobs:
           INTEGRATION_RUN_E2E_TEST: "false"
           INTEGRATION_TEST_ARGS: --test.run CleanState/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|RollingUpdateParallelism
           RUN_TALEMU_TESTS: "false"
-          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceDowngrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
+          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceUpgrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
           WITH_DEBUG: "true"
         run: |
           sudo -E make run-integration-test
-      - name: save-talos-logs-artifacts
+      - name: save-integration-test-artifacts
         if: always()
         uses: actions/upload-artifact@v4
         with:
-          name: talos-logs-e2e-scaling
+          name: integration-test-e2e-scaling
           path: |-
             ~/.talos/clusters/**/*.log
             !~/.talos/clusters/**/swtpm.log
-            /tmp/test-logs
+            /tmp/integration-test
           retention-days: "5"
         continue-on-error: true
   e2e-short:
@@ -536,19 +536,19 @@ jobs:
           INTEGRATION_RUN_E2E_TEST: "false"
           INTEGRATION_TEST_ARGS: --test.run CleanState/|TalosImageGeneration/|ImmediateClusterDestruction/|DefaultCluster/|EncryptedCluster/|SinglenodeCluster/|Auth/
           RUN_TALEMU_TESTS: "false"
-          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceDowngrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
+          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceUpgrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
           WITH_DEBUG: "true"
         run: |
           sudo -E make run-integration-test
-      - name: save-talos-logs-artifacts
+      - name: save-integration-test-artifacts
         if: always()
         uses: actions/upload-artifact@v4
         with:
-          name: talos-logs-e2e-short
+          name: integration-test-e2e-short
           path: |-
             ~/.talos/clusters/**/*.log
             !~/.talos/clusters/**/swtpm.log
-            /tmp/test-logs
+            /tmp/integration-test
           retention-days: "5"
         continue-on-error: true
   e2e-short-secureboot:
@@ -616,19 +616,19 @@ jobs:
           INTEGRATION_RUN_E2E_TEST: "false"
           INTEGRATION_TEST_ARGS: --test.run CleanState/|TalosImageGeneration/|ImmediateClusterDestruction/|DefaultCluster/|EncryptedCluster/|SinglenodeCluster/|Auth/
           RUN_TALEMU_TESTS: "false"
-          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceDowngrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
+          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceUpgrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
           WITH_DEBUG: "true"
         run: |
           sudo -E make run-integration-test
-      - name: save-talos-logs-artifacts
+      - name: save-integration-test-artifacts
         if: always()
         uses: actions/upload-artifact@v4
         with:
-          name: talos-logs-e2e-short-secureboot
+          name: integration-test-e2e-short-secureboot
           path: |-
             ~/.talos/clusters/**/*.log
             !~/.talos/clusters/**/swtpm.log
-            /tmp/test-logs
+            /tmp/integration-test
           retention-days: "5"
         continue-on-error: true
   e2e-templates:
@@ -695,19 +695,19 @@ jobs:
           INTEGRATION_RUN_E2E_TEST: "false"
           INTEGRATION_TEST_ARGS: --test.run CleanState/|ClusterTemplate/
           RUN_TALEMU_TESTS: "false"
-          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceDowngrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
+          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceUpgrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
           WITH_DEBUG: "true"
         run: |
           sudo -E make run-integration-test
-      - name: save-talos-logs-artifacts
+      - name: save-integration-test-artifacts
         if: always()
         uses: actions/upload-artifact@v4
         with:
-          name: talos-logs-e2e-templates
+          name: integration-test-e2e-templates
           path: |-
             ~/.talos/clusters/**/*.log
             !~/.talos/clusters/**/swtpm.log
-            /tmp/test-logs
+            /tmp/integration-test
           retention-days: "5"
         continue-on-error: true
   e2e-upgrades:
@@ -772,21 +772,21 @@ jobs:
       - name: run-integration-test
         env:
           INTEGRATION_RUN_E2E_TEST: "false"
-          INTEGRATION_TEST_ARGS: --test.run CleanState/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceDowngrade/
+          INTEGRATION_TEST_ARGS: --test.run CleanState/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceUpgrade/
           RUN_TALEMU_TESTS: "false"
-          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceDowngrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
+          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceUpgrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
           WITH_DEBUG: "true"
         run: |
           sudo -E make run-integration-test
-      - name: save-talos-logs-artifacts
+      - name: save-integration-test-artifacts
         if: always()
         uses: actions/upload-artifact@v4
         with:
-          name: talos-logs-e2e-upgrades
+          name: integration-test-e2e-upgrades
           path: |-
             ~/.talos/clusters/**/*.log
             !~/.talos/clusters/**/swtpm.log
-            /tmp/test-logs
+            /tmp/integration-test
           retention-days: "5"
         continue-on-error: true
   e2e-workload-proxy:
@@ -853,18 +853,18 @@ jobs:
           INTEGRATION_RUN_E2E_TEST: "false"
           INTEGRATION_TEST_ARGS: --test.run CleanState/|WorkloadProxy
           RUN_TALEMU_TESTS: "false"
-          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceDowngrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
+          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceUpgrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
           WITH_DEBUG: "true"
         run: |
           sudo -E make run-integration-test
-      - name: save-talos-logs-artifacts
+      - name: save-integration-test-artifacts
         if: always()
         uses: actions/upload-artifact@v4
         with:
-          name: talos-logs-e2e-workload-proxy
+          name: integration-test-e2e-workload-proxy
           path: |-
             ~/.talos/clusters/**/*.log
             !~/.talos/clusters/**/swtpm.log
-            /tmp/test-logs
+            /tmp/integration-test
           retention-days: "5"
         continue-on-error: true

.github/workflows/e2e-backups-cron.yaml
@@ -1,6 +1,6 @@
 # THIS FILE WAS AUTOMATICALLY GENERATED, PLEASE DO NOT EDIT.
 #
-# Generated on 2025-02-18T13:59:57Z by kres 8a48729.
+# Generated on 2025-05-22T17:54:46Z by kres 9f64b0d.
 
 name: e2e-backups-cron
 concurrency:
@@ -63,17 +63,17 @@ jobs:
           INTEGRATION_RUN_E2E_TEST: "false"
           INTEGRATION_TEST_ARGS: --test.run CleanState/|EtcdBackupAndRestore
           RUN_TALEMU_TESTS: "false"
-          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceDowngrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
+          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceUpgrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
           WITH_DEBUG: "true"
         run: |
           sudo -E make run-integration-test
-      - name: save-talos-logs-artifacts
+      - name: save-integration-test-artifacts
         if: always()
         uses: actions/upload-artifact@v4
         with:
-          name: talos-logs
+          name: integration-test
           path: |-
             ~/.talos/clusters/**/*.log
             !~/.talos/clusters/**/swtpm.log
-            /tmp/test-logs
+            /tmp/integration-test
           retention-days: "5"

.github/workflows/e2e-forced-removal-cron.yaml
@@ -1,6 +1,6 @@
 # THIS FILE WAS AUTOMATICALLY GENERATED, PLEASE DO NOT EDIT.
 #
-# Generated on 2025-02-18T13:59:57Z by kres 8a48729.
+# Generated on 2025-05-22T17:54:46Z by kres 9f64b0d.
 
 name: e2e-forced-removal-cron
 concurrency:
@@ -63,17 +63,17 @@ jobs:
           INTEGRATION_RUN_E2E_TEST: "false"
           INTEGRATION_TEST_ARGS: --test.run CleanState/|ForcedMachineRemoval/|ReplaceControlPlanes/|ConfigPatching/|KubernetesNodeAudit/
           RUN_TALEMU_TESTS: "false"
-          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceDowngrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
+          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceUpgrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
           WITH_DEBUG: "true"
         run: |
           sudo -E make run-integration-test
-      - name: save-talos-logs-artifacts
+      - name: save-integration-test-artifacts
         if: always()
         uses: actions/upload-artifact@v4
         with:
-          name: talos-logs
+          name: integration-test
           path: |-
             ~/.talos/clusters/**/*.log
             !~/.talos/clusters/**/swtpm.log
-            /tmp/test-logs
+            /tmp/integration-test
           retention-days: "5"

.github/workflows/e2e-scaling-cron.yaml
@@ -1,6 +1,6 @@
 # THIS FILE WAS AUTOMATICALLY GENERATED, PLEASE DO NOT EDIT.
 #
-# Generated on 2025-02-18T13:59:57Z by kres 8a48729.
+# Generated on 2025-05-22T17:54:46Z by kres 9f64b0d.
 
 name: e2e-scaling-cron
 concurrency:
@@ -63,17 +63,17 @@ jobs:
           INTEGRATION_RUN_E2E_TEST: "false"
           INTEGRATION_TEST_ARGS: --test.run CleanState/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|RollingUpdateParallelism
           RUN_TALEMU_TESTS: "false"
-          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceDowngrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
+          TALEMU_TEST_ARGS: --test.run ImmediateClusterDestruction/|EncryptedCluster/|SinglenodeCluster/|ScaleUpAndDown/|ScaleUpAndDownMachineClassBasedMachineSets/|TalosUpgrades/|KubernetesUpgrades/|MaintenanceUpgrade/|ClusterTemplate/|ScaleUpAndDownAutoProvisionMachineSets/
           WITH_DEBUG: "true"
         run: |
           sudo -E make run-integration-test
-      - name: save-talos-logs-artifacts
+      - name: save-integration-test-artifacts
         if: always()
         uses: actions/upload-artifact@v4
         with:
-          name: talos-logs
+          name: integration-test
           path: |-
             ~/.talos/clusters/**/*.log
             !~/.talos/clusters/**/swtpm.log
-            /tmp/test-logs
+            /tmp/integration-test
           retention-days: "5"

commit aa5d89d6d41be2b1c97de442b73d9736948f876a

fix: fix panic in maintenance upgrade
Fix the regression introduced in https://github.com/siderolabs/omni/pull/1214.

It was wrong to use `MachineConfigGenOptions` in the new upgrade endpoint, as its `InstallImage` field is populated only if the machine was ever allocated.

So, for example, when using disk images, the machine is installed to disk (maintenance-upgradeable), but the `InstallImage` field is not populated.

Fix this by building the `InstallImage` spec using the `MachineStatus`.

Signed-off-by: Utku Ozdemir <utku.ozdemir@siderolabs.com>

internal/backend/grpc/management.go
@@ -38,6 +38,7 @@ import (
 
 	commonOmni "github.com/siderolabs/omni/client/api/common"
 	"github.com/siderolabs/omni/client/api/omni/management"
+	"github.com/siderolabs/omni/client/api/omni/specs"
 	"github.com/siderolabs/omni/client/pkg/constants"
 	"github.com/siderolabs/omni/client/pkg/omni/resources"
 	omnires "github.com/siderolabs/omni/client/pkg/omni/resources/omni"
@@ -699,17 +700,28 @@ func (s *managementServer) MaintenanceUpgrade(ctx context.Context, req *manageme
 		return nil, status.Error(codes.FailedPrecondition, "machine is not in maintenance mode")
 	}
 
-	machineConfigGenOptions, err := safe.ReaderGetByID[*omnires.MachineConfigGenOptions](ctx, s.omniState, req.MachineId)
-	if err != nil {
-		if state.IsNotFoundError(err) {
-			return nil, status.Error(codes.NotFound, "machine config gen options not found")
-		}
-
-		return nil, fmt.Errorf("failed to get machine config gen options: %w", err)
+	schematic := machineStatus.TypedSpec().Value.Schematic
+	if schematic == nil || schematic.FullId == "" {
+		return nil, status.Error(codes.FailedPrecondition, "machine schematic is not known yet")
 	}
 
-	installImage := machineConfigGenOptions.TypedSpec().Value.InstallImage
-	installImage.TalosVersion = req.Version
+	platform := machineStatus.TypedSpec().Value.GetPlatformMetadata().GetPlatform()
+	if platform == "" {
+		return nil, status.Error(codes.FailedPrecondition, "machine platform is not known yet")
+	}
+
+	securityState := machineStatus.TypedSpec().Value.SecurityState
+	if securityState == nil {
+		return nil, status.Error(codes.FailedPrecondition, "machine security state is not known yet")
+	}
+
+	installImage := &specs.MachineConfigGenOptionsSpec_InstallImage{
+		TalosVersion:         req.Version,
+		SchematicId:          schematic.FullId,
+		SchematicInitialized: true,
+		Platform:             platform,
+		SecurityState:        securityState,
+	}
 
 	installImageStr, err := installimage.Build(s.imageFactoryClient.Host(), req.MachineId, installImage)
 	if err != nil {

internal/backend/installimage/installimage.go
@@ -23,6 +23,10 @@ func Build(imageFactoryHost string, resID resource.ID, installImage *specs.Machi
 		return "", fmt.Errorf("image factory host is not set")
 	}
 
+	if installImage == nil {
+		return "", fmt.Errorf("install image is nil for machine %q", resID)
+	}
+
 	if !installImage.SchematicInitialized {
 		return "", fmt.Errorf("machine %q has no schematic information set", resID)
 	}

internal/backend/runtime/omni/controllers/omni/machine_config_gen_options.go
@@ -70,7 +70,6 @@ func GenInstallConfig(machineStatus *omni.MachineStatus, clusterMachineTalosVers
 
 		genOptions.TypedSpec().Value.InstallImage.SchematicId = clusterMachineTalosVersion.TypedSpec().Value.SchematicId
 		genOptions.TypedSpec().Value.InstallImage.TalosVersion = clusterMachineTalosVersion.TypedSpec().Value.TalosVersion
-		genOptions.TypedSpec().Value.InstallImage.SchematicId = clusterMachineTalosVersion.TypedSpec().Value.SchematicId
 		genOptions.TypedSpec().Value.InstallImage.SchematicInitialized = machineStatus.TypedSpec().Value.Schematic != nil
 
 		if genOptions.TypedSpec().Value.InstallImage.SchematicInitialized {