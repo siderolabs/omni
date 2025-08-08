7111e9fab6b7f4842e552b701980a4ac8d4d7bcd
Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>
2025-08-08 17:12:45 +0200

    chore: ts, lint, and formatting rules for omni frontend
    Update tsconfig and eslint with recommended rules for vue, vite and typescript. Also include prettier for consistent opinionated formatting. Include a little README with basic commands outline. Also add vue devtools to vite config.
    
    Signed-off-by: Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>

---
+++ frontend/src/types/external-libs.d.ts
@@ -0,0 +1,8 @@
+// Copyright (c) 2025 Sidero Labs, Inc.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+
+declare module 'click-outside-vue3'
+declare module 'vue-virtual-scroller'
+declare module 'vue3-clipboard'
