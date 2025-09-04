2e4696d649e8cd8803f40f4c095e7feec4970e29
Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>
2025-09-04 08:54:00 +0200

    chore: refactor routes to remove withPrefix
    Refactor routes to remove withPrefix and instead use vue-router's native children support.
    
    Signed-off-by: Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>

--- frontend/src/types/vue-router.d.ts
+++ frontend/src/types/vue-router.d.ts
@@ -2,6 +2,10 @@
 //
 // Use of this software is governed by the Business Source License
 // included in the LICENSE file.
-declare module 'click-outside-vue3'
-declare module 'vue-virtual-scroller'
-declare module 'vue3-clipboard'
+import 'vue-router'
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    title?: string
+  }
+}
