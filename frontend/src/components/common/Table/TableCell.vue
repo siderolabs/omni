90bd23a13017f23d116a89331bdb34d93b198e79
Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>
2025-10-14 13:55:36 +0200

    feat(frontend): create a generic table component
    Create a generic table component to share table styling
    
    Signed-off-by: Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>

---
+++ frontend/src/components/common/Table/TableCell.vue
@@ -0,0 +1,15 @@
+<!--
+Copyright (c) 2025 Sidero Labs, Inc.
+
+Use of this software is governed by the Business Source License
+included in the LICENSE file.
+-->
+<script setup lang="ts">
+defineProps<{ th?: boolean }>()
+</script>
+
+<template>
+  <component :is="th ? 'th' : 'td'" class="px-2" :class="th ? 'text-uppercase py-2' : 'py-4'">
+    <slot></slot>
+  </component>
+</template>
---
+++ frontend/src/components/common/Table/TableRow.vue
@@ -0,0 +1,13 @@
+<!--
+Copyright (c) 2025 Sidero Labs, Inc.
+
+Use of this software is governed by the Business Source License
+included in the LICENSE file.
+-->
+<script setup lang="ts"></script>
+
+<template>
+  <tr class="border-naturals-n5 hover:bg-white/5">
+    <slot></slot>
+  </tr>
+</template>
