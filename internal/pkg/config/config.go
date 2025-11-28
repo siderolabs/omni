a1b543be5055269dca514ef544dc6c50e304d845
Utku Ozdemir <utku.ozdemir@siderolabs.com>
2025-11-28 02:01:35 +0100

    feat: store machine logs in sqlite
    Use SQLite storage to store machine logs.
    Use the same SQLite database used for the metrics state.
    
    Refactor machine log storage to define a common interface for circular and sqlite based logs.
    
    Signed-off-by: Utku Ozdemir <utku.ozdemir@siderolabs.com>

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -159,11 +159,17 @@ func Default() *Params {
 				BufferMaxCapacity:     131072,
 				BufferSafetyGap:       256,
 				Storage: LogsMachineStorage{
-					Enabled:             true,
+					Enabled:             false,
 					Path:                "_out/logs",
 					FlushPeriod:         10 * time.Minute,
 					FlushJitter:         0.1,
 					NumCompressedChunks: 5,
+				},
+				SQLite: LogsMachineSQLite{
+					Enabled:          true,
+					Timeout:          10 * time.Second,
+					CleanupInterval:  30 * time.Minute,
+					CleanupOlderThan: 24 * 30 * time.Hour,
 				},
 			},
 		},
