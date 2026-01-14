81c31157288af9bb682f22ab8d996ac9f6977663
Pranav Patil <pranavppatil767@gmail.com>
2026-01-14 08:18:28 +0530

    chore: format/lint fixes

--- internal/backend/runtime/omni/controllers/omni/internal/kubernetes/components.go
+++ internal/backend/runtime/omni/controllers/omni/internal/kubernetes/components.go
@@ -1,18 +1,18 @@
 package kubernetes
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/siderolabs/talos/pkg/machinery/constants"
+	"github.com/siderolabs/talos/pkg/machinery/constants"
 )
 
 // GetComponentImageRefs returns all Kubernetes component image references for a version.
 func GetComponentImageRefs(version string) map[string]string {
-    return map[string]string{
-        "kube-apiserver":          fmt.Sprintf("%s:v%s", constants.KubernetesAPIServerImage, version),
-        "kube-controller-manager": fmt.Sprintf("%s:v%s", constants.KubernetesControllerManagerImage, version),
-        "kube-scheduler":          fmt.Sprintf("%s:v%s", constants.KubernetesSchedulerImage, version),
-        "kubelet":                 fmt.Sprintf("%s:v%s", constants.KubeletImage, version),
-        "kube-proxy":              fmt.Sprintf("%s:v%s", constants.KubeProxyImage, version),
-    }
+	return map[string]string{
+		"kube-apiserver":          fmt.Sprintf("%s:v%s", constants.KubernetesAPIServerImage, version),
+		"kube-controller-manager": fmt.Sprintf("%s:v%s", constants.KubernetesControllerManagerImage, version),
+		"kube-scheduler":          fmt.Sprintf("%s:v%s", constants.KubernetesSchedulerImage, version),
+		"kubelet":                 fmt.Sprintf("%s:v%s", constants.KubeletImage, version),
+		"kube-proxy":              fmt.Sprintf("%s:v%s", constants.KubeProxyImage, version),
+	}
 }
