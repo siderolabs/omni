d7d54916ebac6c8d414cf69467f834057b00f48d
Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>
2026-02-16 18:21:38 +0100

    refactor(frontend): remove <watch> from backupslist
    Remove <Watch> usage from BackupsList. There was also an incorrect docs link and a potential error that was not being displayed, both fixed.
    
    Signed-off-by: Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>

--- frontend/src/views/cluster/Backups/BackupsList.vue
+++ frontend/src/views/cluster/Backups/BackupsList.vue
@@ -6,11 +6,11 @@ included in the LICENSE file.
 -->
 <script setup lang="ts">
 import { useClipboard } from '@vueuse/core'
-import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import WordHighlighter from 'vue-word-highlighter'
 
 import { Runtime } from '@/api/common/omni.pb'
+import type { EtcdBackupOverallStatusSpec, EtcdBackupStatusSpec } from '@/api/omni/specs/omni.pb'
 import {
   EtcdBackupOverallStatusID,
   EtcdBackupOverallStatusType,
@@ -24,135 +24,154 @@ import IconButton from '@/components/common/Button/IconButton.vue'
 import TButton from '@/components/common/Button/TButton.vue'
 import TList from '@/components/common/List/TList.vue'
 import TListItem from '@/components/common/List/TListItem.vue'
-import Watch from '@/components/common/Watch/Watch.vue'
+import TSpinner from '@/components/common/Spinner/TSpinner.vue'
 import TAlert from '@/components/TAlert.vue'
 import { formatBytes, getDocsLink } from '@/methods'
 import { canManageBackupStore } from '@/methods/auth'
 import { formatISO } from '@/methods/time'
+import { useResourceWatch } from '@/methods/useResourceWatch'
 
 const dateFormat = 'HH:mm MMM d y'
 const route = useRoute()
 const { copy } = useClipboard()
 
-const sortOptions = [
-  { id: 'id', desc: 'Creation Time ⬇', descending: true },
-  { id: 'id', desc: 'Creation Time ⬆' },
-]
-
-const watchStatusOpts = computed(() => {
-  return {
-    resource: {
-      namespace: MetricsNamespace,
-      type: EtcdBackupStatusType,
-      id: route.params.cluster as string,
-    },
-    runtime: Runtime.Omni,
-  } as const
+const {
+  data: etcdBackupOverallStatus,
+  loading: etcdBackupOverallStatusLoading,
+  err: etcdBackupOverallStatusErr,
+} = useResourceWatch<EtcdBackupOverallStatusSpec>({
+  resource: {
+    namespace: MetricsNamespace,
+    type: EtcdBackupOverallStatusType,
+    id: EtcdBackupOverallStatusID,
+  },
+  runtime: Runtime.Omni,
 })
 
-const watchOverallStatusOpts = computed(() => {
-  return {
-    resource: {
-      namespace: MetricsNamespace,
-      type: EtcdBackupOverallStatusType,
-      id: EtcdBackupOverallStatusID,
-    },
-    runtime: Runtime.Omni,
-  } as const
-})
-
-const watchOpts = computed(() => {
-  return {
-    resource: {
-      namespace: ExternalNamespace,
-      type: EtcdBackupType,
-    },
-    runtime: Runtime.Omni,
-    selectors: [`${LabelCluster}=${route.params.cluster}`],
-  } as const
-})
-
-const docsLink = getDocsLink('omni', '/how-to-guides/etcd-backups#s3-configuration')
-const openDocs = () => window.open(docsLink, '_blank')?.focus()
+const {
+  data: etcdBackupStatus,
+  loading: etcdBackupStatusLoading,
+  err: etcdBackupStatusErr,
+} = useResourceWatch<EtcdBackupStatusSpec>(() => ({
+  skip: !etcdBackupOverallStatus.value || !!etcdBackupOverallStatus.value.spec.configuration_error,
+  resource: {
+    namespace: MetricsNamespace,
+    type: EtcdBackupStatusType,
+    id: route.params.cluster.toString(),
+  },
+  runtime: Runtime.Omni,
+}))
 </script>
 
 <template>
-  <Watch :opts="watchOverallStatusOpts" spinner no-records-alert errors-alert>
-    <template #default="{ data }">
-      <TAlert
-        v-if="data?.spec?.configuration_error"
-        type="warn"
-        :title="`The backups storage is not properly configured: ${data.spec?.configuration_error}`"
+  <div
+    v-if="etcdBackupOverallStatusLoading || etcdBackupStatusLoading"
+    class="flex size-full items-center justify-center"
+  >
+    <TSpinner class="size-6" />
+  </div>
+
+  <TAlert
+    v-else-if="etcdBackupOverallStatusErr || etcdBackupStatusErr"
+    title="Failed to Fetch Data"
+    type="error"
+  >
+    {{ etcdBackupOverallStatusErr || etcdBackupStatusErr }}.
+  </TAlert>
+
+  <TAlert v-else-if="!etcdBackupOverallStatus" type="info" title="No Records">
+    No entries of the requested resource type are found on the server.
+  </TAlert>
+
+  <TAlert
+    v-else-if="etcdBackupOverallStatus.spec.configuration_error"
+    type="warn"
+    :title="`The backups storage is not properly configured: ${etcdBackupOverallStatus.spec.configuration_error}`"
+  >
+    <div class="flex gap-1">
+      Check the
+      <TButton
+        is="a"
+        variant="subtle"
+        size="xs"
+        :href="getDocsLink('omni', '/cluster-management/etcd-backups#s3-configuration')"
+        target="_blank"
+        rel="noopener noreferrer"
       >
-        <div class="flex gap-1">
-          Check the
-          <TButton variant="subtle" size="xs" @click="openDocs">documentation</TButton>
-          on how to configure s3 backups using CLI.
+        documentation
+      </TButton>
+      on how to configure s3 backups using CLI.
+    </div>
+    <div v-if="canManageBackupStore" class="flex gap-1">
+      Or
+      <TButton is="router-link" variant="subtle" size="xs" :to="{ name: 'BackupStorage' }">
+        configure backups in the UI.
+      </TButton>
+    </div>
+  </TAlert>
+
+  <TAlert v-else-if="!etcdBackupStatus" type="info" title="No Records">
+    No entries of the requested resource type are found on the server.
+  </TAlert>
+
+  <template v-else>
+    <TAlert
+      v-if="etcdBackupStatus.spec.error"
+      type="warn"
+      title="There was an issue creating the backup"
+      class="mb-4"
+    >
+      {{ etcdBackupStatus.spec.error }}
+    </TAlert>
+
+    <TList
+      :key="etcdBackupStatus?.metadata.updated"
+      :opts="{
+        resource: {
+          namespace: ExternalNamespace,
+          type: EtcdBackupType,
+        },
+        runtime: Runtime.Omni,
+        selectors: [`${LabelCluster}=${route.params.cluster}`],
+      }"
+      search
+      :sort-options="[
+        { id: 'id', desc: 'Creation Time ⬇', descending: true },
+        { id: 'id', desc: 'Creation Time ⬆' },
+      ]"
+    >
+      <template #default="{ items, searchQuery }">
+        <div class="mb-1 bg-naturals-n2 px-6 py-2 pl-10 text-xs">
+          <div class="grid grid-cols-4 items-center justify-center gap-1 pr-12">
+            <div>ID</div>
+            <div>Creation Date</div>
+            <div>Size</div>
+            <div>Snapshot ID</div>
+          </div>
         </div>
-        <div v-if="canManageBackupStore" class="flex gap-1">
-          Or
-          <TButton variant="subtle" size="xs" @click="$router.push({ name: 'BackupStorage' })">
-            configure backups in the UI.
-          </TButton>
-        </div>
-      </TAlert>
-      <Watch v-else :opts="watchStatusOpts" spinner no-records-alert errors-alert>
-        <template #default="{ data }">
-          <TList
-            :key="data?.metadata?.updated"
-            :opts="watchOpts"
-            search
-            :sort-options="sortOptions"
-          >
-            <template #default="{ items, searchQuery }">
-              <div class="header">
-                <div class="list-grid">
-                  <div>ID</div>
-                  <div>Creation Date</div>
-                  <div>Size</div>
-                  <div>Snapshot ID</div>
-                </div>
+
+        <TListItem v-for="item in items" :key="item.metadata.id!">
+          <div class="relative pr-3 text-naturals-n12" :class="{ 'pl-7': !item.spec.description }">
+            <div class="grid grid-cols-4 items-center justify-center gap-1 pr-12">
+              <WordHighlighter
+                :query="searchQuery"
+                :text-to-highlight="item.metadata.id"
+                highlight-class="bg-naturals-n14"
+              />
+              <div class="text-naturals-n14">
+                {{ formatISO(item.spec.created_at as string, dateFormat) }}
               </div>
-              <TListItem v-for="item in items" :key="item.metadata.id!">
-                <div
-                  class="relative pr-3 text-naturals-n12"
-                  :class="{ 'pl-7': !item.spec.description }"
-                >
-                  <div class="list-grid">
-                    <WordHighlighter
-                      :query="searchQuery"
-                      :text-to-highlight="item.metadata.id"
-                      highlight-class="bg-naturals-n14"
-                    />
-                    <div class="text-naturals-n14">
-                      {{ formatISO(item.spec.created_at as string, dateFormat) }}
-                    </div>
-                    <div class="text-naturals-n14">
-                      {{ formatBytes(parseInt(item.spec.size ?? '0')) }}
-                    </div>
-                    <div class="flex items-center gap-2 text-naturals-n14">
-                      {{ item.spec.snapshot }}
-                      <IconButton icon="copy" @click="copy(item.spec.snapshot)" />
-                    </div>
-                  </div>
-                </div>
-              </TListItem>
-            </template>
-          </TList>
-        </template>
-      </Watch>
-    </template>
-  </Watch>
+              <div class="text-naturals-n14">
+                {{ formatBytes(parseInt(item.spec.size ?? '0')) }}
+              </div>
+              <div class="flex items-center gap-2 text-naturals-n14">
+                {{ item.spec.snapshot }}
+                <IconButton icon="copy" @click="copy(item.spec.snapshot)" />
+              </div>
+            </div>
+          </div>
+        </TListItem>
+      </template>
+    </TList>
+  </template>
 </template>
-
-<style scoped>
-@reference "../../../index.css";
-
-.header {
-  @apply mb-1 bg-naturals-n2 px-6 py-2 pl-10 text-xs;
-}
-
-.list-grid {
-  @apply grid grid-cols-4 items-center justify-center gap-1 pr-12;
-}
-</style>
