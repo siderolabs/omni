2a0d0f4c159785a38cc1946a8c708425a80d11bf
Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>
2026-04-22 12:19:39 +0200

    fix(frontend): clear page state when keys are cleared
    After clearing keys, use location.replace instead of router.replace to do a full page reload to clear any invalid key related state. This addresses an issue where user might see a blank screen if something breaks with their keys whilst still having a valid auth session. Usually an invalid auth session triggers a redirect to the auth provider, causing the same state reset. This also fixes a flakey test in e2e-talemu suite which was suffering from this.
    
    Signed-off-by: Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>
    (cherry picked from commit c98b1187ea98340df3ea74df2bd65563933357e8)

--- frontend/e2e/talemu/auth.spec.ts
+++ frontend/e2e/talemu/auth.spec.ts
@@ -20,13 +20,13 @@ test('Key expiration without existing auth session', async ({ page }) => {
 
   await expect(page.getByRole('heading', { name: 'Home' })).toBeVisible()
 
-  // Invalidate keys
-  await page.evaluate(() => {
-    localStorage.setItem('keyExpirationTime', new Date(0).toISOString())
+  await test.step('Invalidate keys', async () => {
+    await page.evaluate(() => localStorage.setItem('keyExpirationTime', new Date(0).toISOString()))
+
+    // Clear auth0 cookies
+    await page.context().clearCookies()
+    await page.goto('/')
   })
-  // Clear auth0 cookies
-  await page.context().clearCookies()
-  await page.goto('/')
 
   await expect(page.getByText('Log in')).toBeVisible()
 })
@@ -36,16 +36,11 @@ test('Key expiration with existing auth session', async ({ page }) => {
 
   await expect(page.getByRole('heading', { name: 'Home' })).toBeVisible()
 
-  // Invalidate keys
-  await page.evaluate(() => {
-    localStorage.setItem('keyExpirationTime', new Date(0).toISOString())
+  await test.step('Invalidate keys', async () => {
+    await page.evaluate(() => localStorage.setItem('keyExpirationTime', new Date(0).toISOString()))
+    await page.goto('/')
   })
-  await page.goto('/')
 
-  // Need this to make sure getByText polling doesn't miss the auth page
-  await page.waitForURL('**/authenticate**')
-
-  await expect(page.getByText('Authenticate UI Access')).toBeVisible()
   await expect(page.getByRole('heading', { name: 'Home' })).toBeVisible()
 })
 
@@ -54,15 +49,10 @@ test('Escape invalid key state', async ({ page }) => {
 
   await expect(page.getByRole('heading', { name: 'Home' })).toBeVisible()
 
-  // Purposefully break signatures
-  await page.evaluate(() => {
-    localStorage.setItem('publicKeyID', 'fake!')
+  await test.step('Purposefully break signatures', async () => {
+    await page.evaluate(() => localStorage.setItem('publicKeyID', 'fake!'))
+    await page.goto('/')
   })
-  await page.goto('/')
 
-  // Need this to make sure getByText polling doesn't miss the auth page
-  await page.waitForURL('**/authenticate**')
-
-  await expect(page.getByText('Authenticate UI Access')).toBeVisible()
   await expect(page.getByRole('heading', { name: 'Home' })).toBeVisible()
 })
