3cf06b5aa82182e6d15b2601959c4a85d2670557
Noel Georgi <git@frezbo.dev>
2026-05-26 11:33:58 +0530

    fix: fetch versions from registry with auth
    Supports docker config, github token and google auth.
    
    Fixes: #2882
    
    Signed-off-by: Noel Georgi <git@frezbo.dev>
    (cherry picked from commit fa2f11fc0a6402363ddc38adbdee5f2320496890)

--- go.mod
+++ go.mod
@@ -116,6 +116,7 @@ require (
 
 require (
 	cel.dev/expr v0.25.1 // indirect
+	cloud.google.com/go/compute/metadata v0.9.0 // indirect
 	dario.cat/mergo v1.0.2 // indirect
 	filippo.io/hpke v0.4.0 // indirect
 	github.com/Azure/go-ansiterm v0.0.0-20250102033503-faa5f7b0171c // indirect
--- internal/pkg/registry/registry.go
+++ internal/pkg/registry/registry.go
@@ -8,21 +8,35 @@ package registry
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/authn/github"
 	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/google"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 )
 
-// UpgradeCandidates fetches release tags from the Github.
+// UpgradeCandidates fetches all tags from the given container registry source and returns them as a list of strings.
 func UpgradeCandidates(ctx context.Context, source string) ([]string, error) {
 	repo, err := name.NewRepository(source)
 	if err != nil {
 		return nil, err
 	}
 
-	tags, err := remote.List(repo, remote.WithContext(ctx))
+	tags, err := remote.List(
+		repo,
+		remote.WithContext(ctx),
+		remote.WithAuthFromKeychain(
+			authn.NewMultiKeychain(
+				authn.DefaultKeychain,
+				github.Keychain,
+				google.Keychain,
+			),
+		),
+	)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("fetching tags from registry %s: %w", repo.String(), err)
 	}
 
 	return tags, nil
