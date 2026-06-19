498e8c0b4217e650ee4da4e778f7bfc1d0679b1e
Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>
2026-06-19 15:54:32 +0200

    feat(frontend): show error if machine not part of cluster
    On the clustered machine page, if the machine is not part of the cluster being referenced, show an error.
    
    Signed-off-by: Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>

--- frontend/src/pages/(authenticated)/clusters/[cluster]/machine/[machine].vue
+++ frontend/src/pages/(authenticated)/clusters/[cluster]/machine/[machine].vue
@@ -9,15 +9,22 @@ import { computed } from 'vue'
 import { RouterLink, useRoute } from 'vue-router'
 
 import { Runtime } from '@/api/common/omni.pb'
+import type { MachineStatusSpec } from '@/api/omni/specs/omni.pb'
 import {
+  DefaultNamespace,
+  LabelCluster,
+  MachineStatusType,
   TalosConfigNamespace,
   TalosKubespanConfigID,
   TalosKubeSpanConfigType,
 } from '@/api/resources'
 import type { ConfigSpec } from '@/api/talos/kubespan.pb'
+import PageContainer from '@/components/PageContainer/PageContainer.vue'
+import TSpinner from '@/components/Spinner/TSpinner.vue'
 import TabButton from '@/components/Tabs/TabButton.vue'
 import TabContent from '@/components/Tabs/TabContent.vue'
 import Tabs from '@/components/Tabs/Tabs.vue'
+import TAlert from '@/components/TAlert.vue'
 import { useClusterPermissions } from '@/methods/auth'
 import { useResourceWatch } from '@/methods/useResourceWatch'
 import NodesHeader from '@/views/Nodes/NodesHeader.vue'
@@ -25,9 +32,25 @@ import NodesHeader from '@/views/Nodes/NodesHeader.vue'
 definePage({ name: 'NodeDetails' })
 
 const route = useRoute()
-const machine = computed(() => route.params.machine)
+
+const clusterId = computed(() => route.params.cluster)
+const machineId = computed(() => route.params.machine)
+
 const { canReadMachineConfig, canReadConfigPatches, canReadMachinePendingUpdates } =
-  useClusterPermissions(() => route.params.cluster)
+  useClusterPermissions(clusterId)
+
+const { data: machine, loading: machineLoading } = useResourceWatch<MachineStatusSpec>(() => ({
+  runtime: Runtime.Omni,
+  resource: {
+    type: MachineStatusType,
+    namespace: DefaultNamespace,
+    id: machineId.value,
+  },
+}))
+
+const isPartOfCluster = computed(
+  () => machine.value?.metadata.labels?.[LabelCluster] === clusterId.value,
+)
 
 const { data: kubeSpanConfig } = useResourceWatch<ConfigSpec>(() => ({
   runtime: Runtime.Talos,
@@ -37,8 +60,8 @@ const { data: kubeSpanConfig } = useResourceWatch<ConfigSpec>(() => ({
     id: TalosKubespanConfigID,
   },
   context: {
-    cluster: route.params.cluster,
-    machine: route.params.machine,
+    cluster: clusterId.value,
+    machine: machineId.value,
   },
 }))
 
@@ -46,52 +69,52 @@ const routes = computed(() => {
   return [
     {
       name: 'Overview',
-      to: { name: 'NodeOverview', params: { machine: machine.value } },
+      to: { name: 'NodeOverview', params: { machine: machineId.value } },
     },
     {
       name: 'Monitor',
-      to: { name: 'NodeMonitor', params: { machine: machine.value } },
+      to: { name: 'NodeMonitor', params: { machine: machineId.value } },
     },
     {
       name: 'Logs',
-      to: { name: 'NodeLogs', params: { machine: machine.value } },
+      to: { name: 'NodeLogs', params: { machine: machineId.value } },
     },
     {
       name: 'Config',
-      to: { name: 'NodeConfig', params: { machine: machine.value } },
+      to: { name: 'NodeConfig', params: { machine: machineId.value } },
       disabled: !canReadMachineConfig.value,
     },
     {
       name: 'Pending Updates',
-      to: { name: 'NodePendingUpdates', params: { machine: machine.value } },
+      to: { name: 'NodePendingUpdates', params: { machine: machineId.value } },
       disabled: !canReadMachinePendingUpdates.value,
     },
     {
       name: 'Config History',
-      to: { name: 'NodeConfigDiffs', params: { machine: machine.value } },
+      to: { name: 'NodeConfigDiffs', params: { machine: machineId.value } },
       disabled: !canReadMachineConfig.value,
     },
     {
       name: 'Patches',
-      to: { name: 'NodePatches', params: { machine: machine.value } },
+      to: { name: 'NodePatches', params: { machine: machineId.value } },
       disabled: !canReadConfigPatches.value,
     },
     {
       name: 'KubeSpan',
-      to: { name: 'NodeKubeSpanStatus', params: { machine: machine.value } },
+      to: { name: 'NodeKubeSpanStatus', params: { machine: machineId.value } },
       disabled: !kubeSpanConfig.value?.spec.enabled,
     },
     {
       name: 'Disks',
-      to: { name: 'NodeDisks', params: { machine: machine.value } },
+      to: { name: 'NodeDisks', params: { machine: machineId.value } },
     },
     {
       name: 'Devices',
-      to: { name: 'NodeDevices', params: { machine: machine.value } },
+      to: { name: 'NodeDevices', params: { machine: machineId.value } },
     },
     {
       name: 'Extensions',
-      to: { name: 'NodeExtensions', params: { machine: machine.value } },
+      to: { name: 'NodeExtensions', params: { machine: machineId.value } },
     },
     {
       name: 'Kernel Args',
@@ -102,12 +125,8 @@ const routes = computed(() => {
 </script>
 
 <template>
-  <div class="flex h-full flex-col pt-6">
-    <NodesHeader
-      :cluster-id="$route.params.cluster"
-      :machine-id="$route.params.machine"
-      class="px-4 md:px-6"
-    />
+  <div v-if="machine && isPartOfCluster" class="flex h-full flex-col pt-6">
+    <NodesHeader :cluster-id="clusterId" :machine-id="machineId" class="px-4 md:px-6" />
 
     <Tabs
       :model-value="$route.name?.toString()"
@@ -139,4 +158,16 @@ const routes = computed(() => {
       </template>
     </Tabs>
   </div>
+
+  <PageContainer v-else class="font-sm flex-1">
+    <TSpinner v-if="machineLoading" class="size-5" />
+
+    <TAlert v-else-if="!machine" title="Machine Not Found" type="error">
+      Machine {{ machineId }} does not exist
+    </TAlert>
+
+    <TAlert v-else-if="!isPartOfCluster" title="Invalid machine" type="error">
+      Machine {{ machineId }} is not a member of cluster {{ clusterId }}
+    </TAlert>
+  </PageContainer>
 </template>
