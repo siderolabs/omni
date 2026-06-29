094b25913e8ed273972b13f60c617b97077bd0e4
Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>
2026-06-29 13:37:29 +0200

    feat(frontend): add content-class support to confirm modal
    Add content-class support to the ConfirmModal, and make the center content scrollable, bringing it inline with Modal.
    
    Signed-off-by: Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>

--- frontend/src/components/Modals/ConfirmModal.vue
+++ frontend/src/components/Modals/ConfirmModal.vue
@@ -21,6 +21,7 @@ import {
 
 import TButton from '@/components/Button/TButton.vue'
 import TSpinner from '@/components/Spinner/TSpinner.vue'
+import { cn } from '@/methods/utils'
 
 const props = withDefaults(
   // eslint-disable-next-line vue/define-props-destructuring
@@ -30,6 +31,7 @@ const props = withDefaults(
       actionLabel?: string
       loading?: boolean
       disabled?: boolean
+      contentClass?: string
     }
   >(),
   {
@@ -40,7 +42,14 @@ const props = withDefaults(
 
 const emit = defineEmits<AlertDialogEmits & { confirm: [] }>()
 
-const alertDialogRootProps = reactiveOmit(props, 'title', 'actionLabel', 'loading', 'disabled')
+const alertDialogRootProps = reactiveOmit(
+  props,
+  'title',
+  'actionLabel',
+  'loading',
+  'disabled',
+  'contentClass',
+)
 const forwarded = useForwardPropsEmits(alertDialogRootProps, emit)
 </script>
 
@@ -54,16 +63,18 @@ const forwarded = useForwardPropsEmits(alertDialogRootProps, emit)
       <AlertDialogContent
         class="fixed inset-0 z-30 m-auto flex h-max max-h-screen w-max max-w-screen flex-col rounded-sm bg-naturals-n3 p-8 zoom-in-75 zoom-out-75 fade-in fade-out data-[state=closed]:animate-out data-[state=open]:animate-in"
       >
-        <div class="mb-5 flex flex-col">
+        <div class="mb-5 flex shrink-0 flex-col">
           <AlertDialogTitle class="font-medium text-naturals-n14">{{ title }}</AlertDialogTitle>
           <AlertDialogDescription v-if="$slots.description" class="text-sm">
             <slot name="description"></slot>
           </AlertDialogDescription>
         </div>
 
-        <slot></slot>
+        <div :class="cn('min-h-0 grow overflow-y-auto', contentClass)">
+          <slot></slot>
+        </div>
 
-        <div class="mt-8 flex items-center justify-end gap-2">
+        <div class="mt-8 flex shrink-0 items-center justify-end gap-2">
           <AlertDialogCancel as-child>
             <TButton variant="secondary">Cancel</TButton>
           </AlertDialogCancel>
