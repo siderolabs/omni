84f151fe7298d567fb48a8ff5653fce6b12acb9a
Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>
2026-07-07 15:34:33 +0200

    fix(frontend): handle aborted requests in useMachineServices
    Handle aborted request errors in useMachineServices which is a normal part of closing a stream.
    
    Signed-off-by: Edward Sammut Alessi <edward.sammutalessi@siderolabs.com>

--- frontend/src/methods/useMachineServices.ts
+++ frontend/src/methods/useMachineServices.ts
@@ -10,6 +10,7 @@ import type { RuntimeContext } from '@/api/options'
 import { withAbortController, withContext, withRuntime } from '@/api/options'
 import { MachineService, type ServiceEvent } from '@/api/talos/machine/machine.pb'
 import { TCommonStatuses } from '@/constants'
+import { showError } from '@/notification'
 
 interface Service {
   name?: string
@@ -29,25 +30,31 @@ export function useMachineServices(context: MaybeRefOrGetter<RuntimeContext>) {
     const abortController = new AbortController()
     onCleanup(() => abortController.abort())
 
-    const { messages = [] } = await MachineService.ServiceList(
-      {},
-      withRuntime(Runtime.Talos),
-      withContext(toValue(context)),
-      withAbortController(abortController),
-    )
+    try {
+      const { messages = [] } = await MachineService.ServiceList(
+        {},
+        withRuntime(Runtime.Talos),
+        withContext(toValue(context)),
+        withAbortController(abortController),
+      )
 
-    services.value = messages.flatMap(({ services = [] }) =>
-      services.map((service) => ({
-        name: service.id,
-        state: service.state,
-        status: service.health?.unknown
-          ? TCommonStatuses.HEALTH_UNKNOWN
-          : service.health?.healthy
-            ? TCommonStatuses.HEALTHY
-            : TCommonStatuses.UNHEALTHY,
-        events: service.events?.events,
-      })),
-    )
+      services.value = messages.flatMap(({ services = [] }) =>
+        services.map((service) => ({
+          name: service.id,
+          state: service.state,
+          status: service.health?.unknown
+            ? TCommonStatuses.HEALTH_UNKNOWN
+            : service.health?.healthy
+              ? TCommonStatuses.HEALTHY
+              : TCommonStatuses.UNHEALTHY,
+          events: service.events?.events,
+        })),
+      )
+    } catch (e) {
+      if (abortController.signal.aborted) return
+
+      showError('Error', e instanceof Error ? e.message : String(e))
+    }
   })
 
   watchEffect((onCleanup) => {
